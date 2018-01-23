7d430fa1f16dd7ff20f090b565cc5e8b758b71e5
Jaq Dunham <jdunham9@gmail.com>
2018-01-23 00:12:47 -0500

    doodle function, color picker, clear/undo, basic layout of main page

--- public/js/paperScript.js
+++ public/js/paperScript.js
@@ -1,18 +1,57 @@
-var path = new Path();
-path.strokeColor = 'black';
+var path;
+var color = 'black';
 
 function onMouseDown(event) {
-  path.add(event.point);
-};
+  path = new Path();
+  path.strokeColor = color;
+  path.strokeWidth = 5;
+}
 
-// function onMouseDown(event) {
-// 	console.log('You pressed the mouse!');
-// }
-//
-// function onMouseDrag(event) {
-// 	console.log('You dragged the mouse!');
-// }
-//
-// function onMouseUp(event) {
-// 	console.log('You released the mouse!');
-// }
+function onMouseDrag(event) {
+  path.add(event.point);
+}
+
+$('#save-button').click(function() {
+  var doodle = project.exportSVG({ asString: true });
+  console.log(doodle);
+});
+
+$('#clear-button').click(function() {
+  project.clear();
+});
+
+$('#undo-button').click(function() {
+  path.clear();
+});
+
+$('#red').click(function() {
+  color = 'red';
+});
+
+$('#orange').click(function() {
+  color = 'orange';
+});
+
+$('#yellow').click(function() {
+  color = 'yellow';
+});
+
+$('#green').click(function() {
+  color = 'green';
+});
+
+$('#blue').click(function() {
+  color = 'blue';
+});
+
+$('#purple').click(function() {
+  color = 'purple';
+});
+
+$('#pink').click(function() {
+  color = 'hotpink';
+});
+
+$('#black').click(function() {
+  color = 'black';
+});
