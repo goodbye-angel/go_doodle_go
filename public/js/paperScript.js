0096cf6dd3701087133dd07e85382ae3d750f969
Jaq Dunham <jdunham9@gmail.com>
2018-01-23 23:26:51 -0500

    able to add doodle data to db

--- public/js/paperScript.js
+++ public/js/paperScript.js
@@ -12,9 +12,30 @@ function onMouseDrag(event) {
   path.add(event.point);
 }
 
+function post(path, parameters) {
+    var form = $('<form></form>');
+
+    form.attr("method", "post");
+    form.attr("action", path);
+
+    $.each(parameters, function(key, value) {
+        var field = $('<input></input>');
+
+        field.attr("type", "hidden");
+        field.attr("name", key);
+        field.attr("value", value);
+
+        form.append(field);
+    });
+
+    $(document.body).append(form);
+    form.submit();
+}
+
 $('#save-button').click(function() {
   doodleData = project.exportSVG({ asString: true });
   console.log(doodleData);
+  post('/doodles/', { doodle: doodleData });
 });
 
 $('#clear-button').click(function() {
