846468a477c07f731853967fb3e6971cf82fdf31
Jaq Dunham <jdunham9@gmail.com>
2018-01-28 21:31:22 -0500

    changed 'friends' to 'communtity'

--- controllers/doodles.js
+++ controllers/doodles.js
@@ -7,19 +7,19 @@ const Doodles = require('../models/doodles.js');
 
 // ROUTES
 
-// index (friends page)
-router.get('/friends', async (req, res) => {
-  const friendDoodles = await Doodles.find();
-  res.render('doodles/friends.ejs', { friendDoodles, currentUser: req.session.currentuser });
+// community doodles
+router.get('/community', async (req, res) => {
+  const communityDoodles = await Doodles.find();
+  res.render('doodles/community.ejs', { communityDoodles, currentUser: req.session.currentuser });
 });
 
-// index (current user's doodles)
+// current user's doodles
 router.get('/all', async (req, res) => {
   const allDoodles = await Doodles.find();
   res.render('doodles/all.ejs', { allDoodles, currentUser: req.session.currentuser });
 });
 
-// new (main page)
+// new/main page
 router.get('/', async (req, res) => {
   res.render('doodles/new.ejs', { currentUser: req.session.currentuser });
 });
