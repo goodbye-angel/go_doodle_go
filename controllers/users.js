55afee5a8e6956bc9ff0b2ed9ba70ee5d67df7e4
Jaq Dunham <jdunham9@gmail.com>
2018-01-29 15:59:37 -0500

    redirect when trying to register with a username that is already taken

--- controllers/users.js
+++ controllers/users.js
@@ -11,9 +11,15 @@ const User = require('../models/users.js');
 // create user/encrypt password
 router.post('/register', async (req, res) => {
   req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
-  await User.create(req.body);
-  console.log(req.body);
-  res.redirect('/sessions/login');``
+  try {
+    await User.create(req.body);
+  } catch (err) {
+    console.log(err);
+    res.render('users/username-taken.ejs');
+  } finally {
+    console.log(req.body);
+    res.redirect('/sessions/login');
+  }
 });
 
 // new
